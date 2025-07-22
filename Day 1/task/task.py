a710320a096566fa7869d123738330b722755cf3
HwanKR <fabronjeon@naver.com>
2025-07-22 16:25:04 +0900

    Day1-6 Complete

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,11 @@
-name = "Angela"
-length = len(name)
-print(length)
+# print("Welcome to the Band Name Generator.")
+# print("What's the name of the city you grew up in?")
+# city = input()
+# print("What's your pet's name?")
+# pet_name = input()
+# print("Your band name could be " + city + " " + pet_name)
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_name)
