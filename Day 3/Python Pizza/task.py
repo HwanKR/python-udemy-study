acad9a49e0fe97afe54562fbe38e57151c92f030
HwanKR <fabronjeon@naver.com>
2025-07-23 16:07:04 +0900

    Day3-5 Complete

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,36 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+total_price = 0
+
+if size == 'S':
+    total_price += 15
+    # if pepperoni == 'Y':
+    #     total_price += 2
+    # if extra_cheese == 'Y':
+    #     total_price += 1
+elif size == 'M':
+    total_price += 20
+    # if pepperoni == 'Y':
+    #     total_price += 3
+    # if extra_cheese == 'Y':
+    #     total_price += 1
+elif size == 'L':
+    total_price += 25
+    # if pepperoni == 'Y':
+    #     total_price += 3
+    # if extra_cheese == 'Y':
+    #     total_price += 1
+else:
+    print("You typed the wrong inputs.")
+
+if pepperoni == 'Y':
+    if size == 'S':
+        total_price += 2
+    else:
+        total_price += 3
+
+if extra_cheese == 'Y':
+    total_price += 1
+
+print(f"Your final bill is: ${total_price}.")
