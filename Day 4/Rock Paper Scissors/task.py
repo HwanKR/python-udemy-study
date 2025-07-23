89c5d4f8f9e1deae59359f8a5104be00a1cd7498
HwanKR <fabronjeon@naver.com>
2025-07-23 20:46:16 +0900

    Day4-5 Complete

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,20 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+game_array = [rock, paper, scissors]
+
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+computer_input = random.randint(0, 2)
+fight = user_input-computer_input
+
+print(game_array[user_input])
+print(f"Computer chose: {computer_input}")
+print(game_array[computer_input])
+
+if user_input == computer_input:
+    print("draw")
+elif fight == -2 or fight == 1:
+    print("You win")
+else:
+    print("You lose")
