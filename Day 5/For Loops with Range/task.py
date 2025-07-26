e15bc697740512d1e86262b2de5c5ac043db9b48
HwanKR <fabronjeon@naver.com>
2025-07-26 12:42:18 +0900

    Day5-4 Complete

--- Day 5/For Loops with Range/task.py
+++ Day 5/For Loops with Range/task.py
@@ -3,3 +3,14 @@ for number in range(1, 101):
     sum += number
 
 print(sum)
+
+# for number in range(1, 101):
+#     if number >= 15 and (number % 15 == 0):
+#         print("FizzBuzz")
+#     elif number >= 3 and (number % 3 == 0):
+#         print("Fizz")
+#     elif number >= 5 and (number % 5 == 0):
+#         print("Buzz")
+#     else:
+#         print(number)
+
