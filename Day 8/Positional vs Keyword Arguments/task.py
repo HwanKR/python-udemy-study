e8def5834d72e3cca2452f204cdb2e8c558cdeac
HwanKR <fabronjeon@naver.com>
2025-08-20 23:48:51 +0900

    Day8-2 Complete

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,14 @@
 # Functions with input
 
-def greet_with_name(name):
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
+    print(f"What is it like in {location}?")
 
-
-greet_with_name("Jack Bauer")
+greet_with(location='Daegue', name='Hwan')
