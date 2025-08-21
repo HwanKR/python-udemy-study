a19e8dd95b646c32077639d78896a32788948b2a
HwanKR <fabronjeon@naver.com>
2025-08-21 17:58:04 +0900

    Day8-3 Complete

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -1,11 +1,22 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    encrypt_word = ""
+    for letter in original_text:
+        new_index = 0
+        if alphabet.index(letter)+shift_amount > 26:
+            new_index = alphabet.index(letter)+shift_amount -26
+        else:
+            new_index = alphabet.index(letter)+shift_amount
+        new_letter = alphabet[new_index]
+        encrypt_word += new_letter
+    print(encrypt_word)
 
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
@@ -15,3 +26,4 @@ shift = int(input("Type the shift number:\n"))
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
 
+encrypt(text, shift)
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -7,8 +7,29 @@
 #
 # greet_with_name("Jack Bauer")
 
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
+#
+# greet_with(location='Daegue', name='Hwan')
 
-greet_with(location='Daegue', name='Hwan')
+def calculate_love_score(name1, name2):
+    word_true = "true"
+    word_love = "love"
+
+    name1 = name1.lower()
+    name2 = name2.lower()
+
+    count_true = 0
+    count_love = 0
+
+    for letter in word_true:
+        count_true = count_true + name1.count(letter) + name2.count(letter)
+
+    for letter in word_love:
+        count_love = count_love + name1.count(letter) + name2.count(letter)
+
+    print(f"{count_true}{count_love}")
+
+
+calculate_love_score("Kanye West", "Kim Kardashian")
